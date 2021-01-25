8bacfd2b617c3071a34260da63020ba5cda0f8f1
Axel Lavoie <axel.lavoie@gmail.com>
2021-01-25 13:05:52 +0100

    Add index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-import { GetServerSideProps, NextPage } from "next";
-import { useRouter } from "next/router";
+import { GetStaticProps, NextPage } from "next";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from "next-i18next";
 
 const Index: NextPage = props => {
-  const router = useRouter()
-  const routeUid = router.query.uid
-
   const { t } = useTranslation(["common"])
   
   return (
     <div>
-      {t('test')} {routeUid}
+      {t('hello')}
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
   props: {
     ...await serverSideTranslations(locale, ["common"]),
   }
